c5caece4dfba1c2a5eb9a711b3127bdc529d51c8
Michael Jackson <mj94@mj94.net>
2017-10-07 16:54:37 -0500

    Eliminate global variable to cleanup scope

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
-    var selectedSoundFileName = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +21,12 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        selectedSoundFileName = soundArray[sender.tag - 1]
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag - 1])
     
     }
     
-    func playSound() {
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
