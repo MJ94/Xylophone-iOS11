3572308daf8b7d3c33cd16442d3344d80ee495b9
Michael Jackson <mj94@mj94.net>
2017-10-07 16:00:34 -0500

    Connect buttons to proper sound files

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,7 +12,8 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
-    
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var selectedSoundFileName = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +22,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
         playSound()
     
     }
     
     func playSound() {
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
